204f3dd393275336fec6381160de412c8edce4c9
ilyamakhon <i.makhon@yandex.ru>
2018-11-28 00:58:31 +0300

    source files reloaded

--- src/main/java/com/epam/ut/runner/Main.java
+++ src/main/java/com/epam/ut/runner/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 
 	public static void main(String[] args) {
+
 		Triangle tr = new Triangle(1.0, 2.0, 2.0);
 		System.out.println(tr.checkTriangle());
 		System.out.println(tr.getMessage());
