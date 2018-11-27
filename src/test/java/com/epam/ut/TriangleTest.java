da57b98914248a3da6bd7b2941f86d9c76fcd3b4
ilyamakhon <i.makhon@yandex.ru>
2018-11-27 21:38:26 +0300

    testng.xml added + test triangle type group added

--- src/test/java/com/epam/ut/TriangleTest.java
+++ src/test/java/com/epam/ut/TriangleTest.java
@@ -6,15 +6,47 @@ import org.testng.annotations.Test;
 
 public class TriangleTest {
 
-    @Test
+    //-----------TEST TRIANGLE TYPE GROUP
+
+    @Test(groups = {"tst_triangle_type"})
     public void tst_equilateral_triangle() {
         Triangle equilateralTriangle = new Triangle(2, 2, 2);
 
         boolean isTriangle = equilateralTriangle.checkTriangle();
 
         Assert.assertTrue(isTriangle);
-        System.out.println(equilateralTriangle.detectTriangle());
-        Assert.assertEquals(1 , equilateralTriangle.detectTriangle());
+        Assert.assertEquals(3 , equilateralTriangle.detectTriangle());
     }
 
+    @Test(groups = {"tst_triangle_type"})
+    public void tst_isosceles_triangle() {
+        Triangle isoscelesTriangle = new Triangle(10, 100, 100);
+
+        boolean isTriangle = isoscelesTriangle.checkTriangle();
+
+        Assert.assertTrue(isTriangle);
+        Assert.assertEquals(2 , isoscelesTriangle.detectTriangle());
+    }
+
+    @Test(groups = {"tst_triangle_type"})
+    public void tst_ordinary_triangle() {
+        Triangle ordinaryTriangle = new Triangle(10, 6, 5);
+
+        boolean isTriangle = ordinaryTriangle.checkTriangle();
+
+        Assert.assertTrue(isTriangle);
+        Assert.assertEquals(4 , ordinaryTriangle.detectTriangle());
+    }
+
+    @Test(groups = {"tst_triangle_type"})
+    public void tst_rectangular_triangle() {
+        Triangle rectangularTriangle = new Triangle(3, 4, 5);
+
+        boolean isTriangle = rectangularTriangle.checkTriangle();
+
+        Assert.assertTrue(isTriangle);
+        Assert.assertEquals(8 , rectangularTriangle.detectTriangle());
+    }
+
+    //-----------TEST TRIANGLE TYPE GROUP
 }
