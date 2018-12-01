c9db234ee2c5113aa2d1059a002c48499b8bfddc
ilyamakhon <i.makhon@yandex.ru>
2018-12-01 18:19:49 +0300

    tests groups and tests added, parametrization fixes

--- src/test/java/com/epam/ut/TriangleTest.java
+++ src/test/java/com/epam/ut/TriangleTest.java
@@ -1,215 +1,153 @@
 package com.epam.ut;
 
+import com.epam.ut.dataprovider.DataProviderClass;
 import com.epam.ut.triangle.Triangle;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.lang.reflect.Method;
 
 public class TriangleTest {
 
     private boolean isTriangle;
 
-    @DataProvider
-    public Object[][] dataProvider(Method method)
-    {
-        switch (method.getName()) {
-            //----------------TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | POSITIVE TESTS
-            case "tst_positive_equilateral_triangle" :
-                return new Object[][] {{new Triangle(2,2,2)}};
-            case "tst_positive_isosceles_triangle" :
-                return new Object[][] {{new Triangle(10, 100, 100)}};
-            case "tst_positive_ordinary_triangle" :
-                return new Object[][] {{new Triangle(10, 6, 5)}};
-            case "tst_positive_rectangular_triangle" :
-                return new Object[][] {{new Triangle(3, 4, 5)}};
-            //----------------TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | POSITIVE TESTS
 
-            //----------------TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | NEGATIVE TESTS
-            case "tst_negative_equilateral_triangle" :
-                return new Object[][] {
-                    {new Triangle(-2,-2,-2)},
-                    {new Triangle(2,-2,-2)},
-                    {new Triangle(-2,2,-2)},
-                    {new Triangle(-2,-2,2)}};
-
-            case "tst_negative_isosceles_triangle" :
-                return new Object[][] {
-                    {new Triangle(-10, -100, -100)},
-                    {new Triangle(10, -100, -100)},
-                    {new Triangle(-10, 100, -100)},
-                    {new Triangle(-10, -100, 100)}};
-            case "tst_negative_ordinary_triangle" :
-                return new Object[][] {
-                    {new Triangle(-10, -6, -5)},
-                    {new Triangle(10, -6, -5)},
-                    {new Triangle(-10, 6, -5)},
-                    {new Triangle(-10, -6, 5)}};
-            case "tst_negative_rectangular_triangle" :
-                return new Object[][] {
-                    {new Triangle(-3, -4, -5)},
-                    {new Triangle(3, -4, -5)},
-                    {new Triangle(-3, 4, -5)},
-                    {new Triangle(-3, -4, 5)}};
-            //----------------TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | NEGATIVE TESTS
-
-            //----------------TEST DATA FOR TRIANGLE CHECKER TEST GROUP | POSITIVE TESTS
-            case "tst_all_positive_sides_handling" :
-                return new Object[][] {{new Triangle(3, 4, 5)}};
-            case "tst_a_plus_b_side_greater_than_c_side_handling" :
-                return new Object[][] {{new Triangle(4, 4, 6)}};
-            case "tst_a_plus_c_side_greater_than_b_side_handling" :
-                return new Object[][] {{new Triangle(4, 6, 4)}};
-            case "tst_b_plus_c_side_greater_than_a_side_handling" :
-                return new Object[][] {{new Triangle(6, 4, 4)}};
-            //----------------TEST DATA FOR TRIANGLE CHECKER TEST GROUP | POSITIVE TESTS
-
-            //----------------TEST DATA FOR TRIANGLE CHECKER TEST GROUP | NEGATIVE TESTS
-            case "tst_negative_a_side_handling" :
-                return new Object[][] {{new Triangle(-3, 4, 5)}};
-            case "tst_negative_b_side_handling" :
-                return new Object[][] {{new Triangle(3, -4, 5)}};
-            case "tst_negative_c_side_handling" :
-                return new Object[][] {{new Triangle(3, 4, -5)}};
-            case "tst_all_negative_sides_handling" :
-                return new Object[][] {{new Triangle(-3, -4, -5)}};
-            case "tst_a_plus_b_side_less_than_c_side_handling" :
-                return new Object[][] {{new Triangle(4, 4, 10)}};
-            case "tst_a_plus_c_side_less_than_b_side_handling" :
-                return new Object[][] {{new Triangle(4, 10, 4)}};
-            case "tst_b_plus_c_side_less_than_a_side_handling" :
-                return new Object[][] {{new Triangle(10, 4, 4)}};
-            //----------------TEST DATA FOR TRIANGLE CHECKER TEST GROUP | NEGATIVE TESTS
-
-        }
-        return new Object[][]{};
-    }
 
     //----------------------TEST TRIANGLE CHECKER GROUP
 
     //-----------POSITIVE TESTS
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_positive_group"})
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_positive_group"})
     public void tst_all_positive_sides_handling(Triangle allSidesPositiveValueTriangle) {
         isTriangle = allSidesPositiveValueTriangle.checkTriangle();
         Assert.assertTrue(isTriangle);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_positive_group"})
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_positive_group"})
     public void tst_a_plus_b_side_greater_than_c_side_handling(Triangle aPlusBSideGreaterThanCSideTriangle) {
         isTriangle = aPlusBSideGreaterThanCSideTriangle.checkTriangle();
         Assert.assertTrue(isTriangle);
     }
 
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_positive_group"})
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_positive_group"})
     public void tst_a_plus_c_side_greater_than_b_side_handling(Triangle aPlusCSideGreaterThanBSideTriangle) {
         isTriangle = aPlusCSideGreaterThanBSideTriangle.checkTriangle();
         Assert.assertTrue(isTriangle);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_positive_group"})
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_positive_group"})
     public void tst_b_plus_c_side_greater_than_a_side_handling(Triangle bPlusCSideGreaterThanASideTriangle) {
         isTriangle = bPlusCSideGreaterThanASideTriangle.checkTriangle();
         Assert.assertTrue(isTriangle);
     }
-    //-----------POSITIVE TESTS
+    //+++++++++++POSITIVE TESTS
 
     //-----------NEGATIVE TESTS
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
     public void tst_negative_a_side_handling(Triangle aSideNegativeTriangle) {
         isTriangle = aSideNegativeTriangle.checkTriangle();
         Assert.assertFalse(isTriangle);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
     public void tst_negative_b_side_handling(Triangle bSideNegativeTriangle) {
         isTriangle = bSideNegativeTriangle.checkTriangle();
         Assert.assertFalse(isTriangle);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
     public void tst_negative_c_side_handling(Triangle cSideNegativeTriangle) {
         isTriangle = cSideNegativeTriangle.checkTriangle();
         Assert.assertFalse(isTriangle);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
     public void tst_all_negative_sides_handling(Triangle allSidesNegativeValueTriangle) {
         isTriangle = allSidesNegativeValueTriangle.checkTriangle();
         Assert.assertFalse(isTriangle);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
     public void tst_a_plus_b_side_less_than_c_side_handling(Triangle aPlusBSideLessThanCSideTriangle) {
         isTriangle = aPlusBSideLessThanCSideTriangle.checkTriangle();
-        Assert.assertEquals("a+b<=c", aPlusBSideLessThanCSideTriangle.getMessage());
         Assert.assertFalse(isTriangle);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
     public void tst_a_plus_c_side_less_than_b_side_handling(Triangle aPlusCSideLessThanBSideTriangle) {
         isTriangle = aPlusCSideLessThanBSideTriangle.checkTriangle();
-        Assert.assertEquals("a+c<=b", aPlusCSideLessThanBSideTriangle.getMessage());
         Assert.assertFalse(isTriangle);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_checker_negative_group"}, expectedExceptions = Exception.class)
     public void tst_b_plus_c_side_less_than_a_side_handling(Triangle bPlusCSideLessThanASideTriangle) {
         isTriangle = bPlusCSideLessThanASideTriangle.checkTriangle();
-        Assert.assertEquals("b+c<=a", bPlusCSideLessThanASideTriangle.getMessage());
         Assert.assertFalse(isTriangle);
     }
-    //-----------NEGATIVE TESTS
+    //+++++++++++NEGATIVE TESTS
 
-    //----------------------TEST TRIANGLE CHECKER GROUP
+    //++++++++++++++++++++++TEST TRIANGLE CHECKER GROUP
 
     //----------------------TEST TRIANGLE DETECTOR GROUP
 
     //-----------POSITIVE TESTS
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_detector_positive_group"})
-    public void tst_positive_equilateral_triangle(Triangle equilateralTriangle) {
-        Assert.assertEquals(1 , equilateralTriangle.detectTriangle());
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_detector_positive_group"})
+    public void tst_positive_equilateral_triangle(Triangle equilateralTriangle, int expectedEquilateral) {
+        Assert.assertEquals(equilateralTriangle.detectTriangle(), expectedEquilateral);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_detector_positive_group"})
-    public void tst_positive_isosceles_triangle(Triangle isoscelesTriangle) {
-        Assert.assertEquals(2 , isoscelesTriangle.detectTriangle());
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_detector_positive_group"})
+    public void tst_positive_isosceles_triangle(Triangle isoscelesTriangle, int expectedIsosceles) {
+        Assert.assertEquals(isoscelesTriangle.detectTriangle(), expectedIsosceles);
     }
     
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_detector_positive_group"})
-    public void tst_positive_ordinary_triangle(Triangle ordinaryTriangle) {
-        Assert.assertEquals(4 , ordinaryTriangle.detectTriangle());
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_detector_positive_group"})
+    public void tst_positive_ordinary_triangle(Triangle ordinaryTriangle, int expectedOrdinary) {
+        Assert.assertEquals(ordinaryTriangle.detectTriangle(), expectedOrdinary);
     }
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_detector_positive_group"})
-    public void tst_positive_rectangular_triangle(Triangle rectangularTriangle) {
-        Assert.assertEquals(8 , rectangularTriangle.detectTriangle());
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_detector_positive_group"})
+    public void tst_positive_rectangular_triangle(Triangle rectangularTriangle, int expectedRectangular) {
+        Assert.assertEquals(rectangularTriangle.detectTriangle(), expectedRectangular);
     }
+    //+++++++++++POSITIVE TESTS
+
+    //-----------NEGATIVE TESTS
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_detector_negative_group"}, expectedExceptions = Exception.class)
+    public void tst_negative_sides_triangle(Triangle equilateralTriangle) {
+        equilateralTriangle.detectTriangle();
+    }
+    //+++++++++++NEGATIVE TESTS
+
+    //++++++++++++++++++++++TEST TRIANGLE DETECTOR GROUP
+
+
+    //----------------------TEST TRIANGLE BASICS GROUP
+
     //-----------POSITIVE TESTS
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_basics_positive_group"})
+    public void tst_constructor(Triangle triangle, double expected, String actualMessage) {
+        double actual = triangle.getSquare();
+        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(triangle.getMessage(), actualMessage);
+    }
+
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_basics_positive_group"})
+    public void tst_square_calculation_with_positive_triangle_sides(double actual, double expected) {
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_basics_positive_group"})
+    public void tst_get_message(String actualMessage, String expectedMessage) {
+        System.out.println(expectedMessage);
+        Assert.assertEquals(actualMessage, expectedMessage);
+    }
+    //+++++++++++POSITIVE TESTS
 
     //-----------NEGATIVE TESTS
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_detector_negative_group"}, expectedExceptions = Exception.class)
-    public void tst_negative_equilateral_triangle(Triangle equilateralTriangle) {
-        Assert.assertEquals(1 , equilateralTriangle.detectTriangle());
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class, groups = {"tst_triangle_basics_negative_group"}, expectedExceptions = Exception.class)
+    public void tst_square_calculation_with_negative_triangle_sides_handling(Triangle triangle) {
+        triangle.getSquare();
     }
+    //+++++++++++NEGATIVE TESTS
 
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_detector_negative_group"}, expectedExceptions = Exception.class)
-    public void tst_negative_isosceles_triangle(Triangle isoscelesTriangle) {
-        Assert.assertEquals(2 , isoscelesTriangle.detectTriangle());
-    }
-
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_detector_negative_group"}, expectedExceptions = Exception.class)
-    public void tst_negative_ordinary_triangle(Triangle ordinaryTriangle) {
-        Assert.assertEquals(4 , ordinaryTriangle.detectTriangle());
-    }
-
-    @Test(dataProvider = "dataProvider", groups = {"tst_triangle_detector_negative_group"}, expectedExceptions = Exception.class)
-    public void tst_negative_rectangular_triangle(Triangle rectangularTriangle) {
-        Assert.assertEquals(8 , rectangularTriangle.detectTriangle());
-    }
-    //-----------NEGATIVE TESTS
-
-    //----------------------TEST TRIANGLE DETECTOR GROUP
-
+    //++++++++++++++++++++++TEST TRIANGLE BASICS GROUP
 }
--- src/test/java/com/epam/ut/dataprovider/DataProviderClass.java
+++ src/test/java/com/epam/ut/dataprovider/DataProviderClass.java
@@ -1,94 +1,134 @@
 package com.epam.ut.dataprovider;
 
 import com.epam.ut.triangle.Triangle;
+import org.testng.annotations.DataProvider;
 
 import java.lang.reflect.Method;
 
 public class DataProviderClass {
-    @org.testng.annotations.DataProvider
+
+    @DataProvider
     public Object[][] dataProvider(Method method)
     {
         switch (method.getName()) {
             //----------------TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | POSITIVE TESTS
             case "tst_positive_equilateral_triangle" :
-                return new Object[][] {{new Triangle(2,2,2)}};
+                return new Object[][] {{ new Triangle(2,2,2), 1 }};
 
             case "tst_positive_isosceles_triangle" :
-                return new Object[][] {{new Triangle(10, 100, 100)}};
+                return new Object[][] {{ new Triangle(10, 100, 100), 2 }};
 
             case "tst_positive_ordinary_triangle" :
-                return new Object[][] {{new Triangle(10, 6, 5)}};
+                return new Object[][] {{ new Triangle(10, 6, 5), 4 }};
 
             case "tst_positive_rectangular_triangle" :
-                return new Object[][] {{new Triangle(3, 4, 5)}};
-            //----------------TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | POSITIVE TESTS
+                return new Object[][] {{ new Triangle(3, 4, 5), 8 }};
+            //++++++++++++++++TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | POSITIVE TESTS
 
             //----------------TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | NEGATIVE TESTS
-            case "tst_negative_equilateral_triangle" :
+            case "tst_negative_sides_triangle" :
                 return new Object[][] {
-                        {new Triangle(-2,-2,-2)},
+                        {new Triangle(-2.5,-2.5,-2.5)},
                         {new Triangle(2,-2,-2)},
                         {new Triangle(-2,2,-2)},
-                        {new Triangle(-2,-2,2)}};
+                        {new Triangle(-2,-2,2)},
 
-            case "tst_negative_isosceles_triangle" :
-                return new Object[][] {
                         {new Triangle(-10, -100, -100)},
-                        {new Triangle(10, -100, -100)},
+                        {new Triangle(10, -100.9, -100.9)},
                         {new Triangle(-10, 100, -100)},
-                        {new Triangle(-10, -100, 100)}};
+                        {new Triangle(-10, -100, 100)},
 
-            case "tst_negative_ordinary_triangle" :
-                return new Object[][] {
                         {new Triangle(-10, -6, -5)},
                         {new Triangle(10, -6, -5)},
                         {new Triangle(-10, 6, -5)},
-                        {new Triangle(-10, -6, 5)}};
+                        {new Triangle(-10, -6, 5)},
 
-            case "tst_negative_rectangular_triangle" :
-                return new Object[][] {
                         {new Triangle(-3, -4, -5)},
                         {new Triangle(3, -4, -5)},
                         {new Triangle(-3, 4, -5)},
                         {new Triangle(-3, -4, 5)}};
-            //----------------TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | NEGATIVE TESTS
+            //++++++++++++++++TEST DATA FOR TRIANGLE DETECTOR TEST GROUP | NEGATIVE TESTS
 
             //----------------TEST DATA FOR TRIANGLE CHECKER TEST GROUP | POSITIVE TESTS
             case "tst_all_positive_sides_handling" :
-                return new Object[][] {{new Triangle(3, 4, 5)}};
+                return new Object[][] { {new Triangle(3, 4, 5)} };
 
             case "tst_a_plus_b_side_greater_than_c_side_handling" :
-                return new Object[][] {{new Triangle(4, 4, 6)}};
+                return new Object[][] { {new Triangle(4, 4, 6)} };
 
             case "tst_a_plus_c_side_greater_than_b_side_handling" :
-                return new Object[][] {{new Triangle(4, 6, 4)}};
+                return new Object[][] { {new Triangle(4, 6, 4)} };
 
             case "tst_b_plus_c_side_greater_than_a_side_handling" :
-                return new Object[][] {{new Triangle(6, 4, 4)}};
-            //----------------TEST DATA FOR TRIANGLE CHECKER TEST GROUP | POSITIVE TESTS
+                return new Object[][] { {new Triangle(6, 4, 4)} };
+            //++++++++++++++++TEST DATA FOR TRIANGLE CHECKER TEST GROUP | POSITIVE TESTS
 
             //----------------TEST DATA FOR TRIANGLE CHECKER TEST GROUP | NEGATIVE TESTS
             case "tst_negative_a_side_handling" :
-                return new Object[][] {{new Triangle(-3, 4, 5)}};
+                return new Object[][] { {new Triangle(-3, 4, 5)} };
 
             case "tst_negative_b_side_handling" :
-                return new Object[][] {{new Triangle(3, -4, 5)}};
+                return new Object[][] { {new Triangle(3, -4, 5)} };
 
             case "tst_negative_c_side_handling" :
-                return new Object[][] {{new Triangle(3, 4, -5)}};
+                return new Object[][] { {new Triangle(3, 4, -5)} };
 
             case "tst_all_negative_sides_handling" :
-                return new Object[][] {{new Triangle(-3, -4, -5)}};
+                return new Object[][] { {new Triangle(-3, -4, -5)} };
 
             case "tst_a_plus_b_side_less_than_c_side_handling" :
-                return new Object[][] {{new Triangle(4, 4, 10)}};
+                return new Object[][] { {new Triangle(4, 4, 10)} };
 
             case "tst_a_plus_c_side_less_than_b_side_handling" :
-                return new Object[][] {{new Triangle(4, 10, 4)}};
+                return new Object[][] { {new Triangle(4, 10, 4)} };
 
             case "tst_b_plus_c_side_less_than_a_side_handling" :
-                return new Object[][] {{new Triangle(10, 4, 4)}};
-            //----------------TEST DATA FOR TRIANGLE CHECKER TEST GROUP | NEGATIVE TESTS
+                return new Object[][] { {new Triangle(10, 4, 4)} };
+            //++++++++++++++++TEST DATA FOR TRIANGLE CHECKER TEST GROUP | NEGATIVE TESTS
+
+
+            //----------------TEST TRIANGLE BASICS GROUP | POSITIVE TESTS
+            case "tst_constructor" :
+                return new Object[][] {{ new Triangle(3, 4, 5), 6.0, ""}};
+
+            case "tst_square_calculation_with_positive_triangle_sides" :
+                Triangle triangle = new Triangle(3, 4, 5);
+                return new Object[][] {{ triangle.getSquare(), 6.0 }};
+
+            case "tst_get_message" :
+                //Setting up triangles for testing getMessage() function
+                Triangle aPlusBSideLessThanCSideTriangle = new Triangle(4, 4, 10);
+                Triangle aPlusCSideLessThanBSideTriangle = new Triangle(4, 10, 4);
+                Triangle bPlusCSideLessThanASideTriangle = new Triangle(10, 4, 4);
+
+                Triangle aSideLessOrEqualsZeroTriangle = new Triangle(-1, 2, 2);
+                Triangle bSideLessOrEqualsZeroTriangle = new Triangle(1, -2, 2);
+                Triangle cSideLessOrEqualsZeroTriangle = new Triangle(1, 2, -2);
+
+                //After checking all triangles message will set upped
+                aPlusBSideLessThanCSideTriangle.checkTriangle();
+                aPlusCSideLessThanBSideTriangle.checkTriangle();
+                bPlusCSideLessThanASideTriangle.checkTriangle();
+
+                aSideLessOrEqualsZeroTriangle.checkTriangle();
+                bSideLessOrEqualsZeroTriangle.checkTriangle();
+                cSideLessOrEqualsZeroTriangle.checkTriangle();
+
+                return new Object[][] {
+                        { aPlusBSideLessThanCSideTriangle.getMessage(), "a+b<=c" },
+                        { aPlusCSideLessThanBSideTriangle.getMessage(), "a+c<=b" },
+                        { bPlusCSideLessThanASideTriangle.getMessage(), "b+c<=a" },
+
+                        { aSideLessOrEqualsZeroTriangle.getMessage(), "a<=0"},
+                        { bSideLessOrEqualsZeroTriangle.getMessage(), "b<=0"},
+                        { cSideLessOrEqualsZeroTriangle.getMessage(), "c<=0"}
+                };
+            //++++++++++++++++TEST TRIANGLE BASICS GROUP | POSITIVE TESTS
+
+            //----------------TEST TRIANGLE BASICS GROUP | NEGATIVE TESTS
+            case "tst_square_calculation_with_negative_triangle_sides_handling" :
+                return new Object[][] {{ new Triangle(-3, -4, -5) }};
+            //++++++++++++++++TEST TRIANGLE BASICS GROUP | NEGATIVE TESTS
 
         }
         return new Object[][]{};
